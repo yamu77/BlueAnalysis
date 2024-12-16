580a6c47d8b4d98f0c914c2074361e9017ce72d4
yamu77 <dokuroshin@gmail.com>
2024-12-16 20:29:32 +0900

    :zap:説明ページ変更

--- src/components/Usage.tsx
+++ src/components/Usage.tsx
@@ -5,6 +5,9 @@ import {
   List,
   ListItem,
   ListItemText,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
 } from "@mui/material";
 import "./Usage.css";
 
@@ -44,7 +47,7 @@ export function Usage() {
           <ListItem>
             <ListItemText
               primary="統計グラフ"
-              secondary="生徒データの分布をグラフで確認できます。グラフはテーブルと連動しており、テーブルのフィルタリングを行うとグラフの表示も更新されます。"
+              secondary="生徒データの分布をグラフで確認できます。"
             />
           </ListItem>
         </List>
@@ -53,16 +56,154 @@ export function Usage() {
         <Typography variant="h5" gutterBottom>
           テーブルについて
         </Typography>
-        <Typography>
-        </Typography>
+        <Typography>生徒の各情報を表示します。各項目で</Typography>
+        <List>
+          <ListItem>
+            <ListItemText primary="フィルタリング" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="ソート" />
+          </ListItem>
+        </List>
+        <Typography>が可能です。</Typography>
+        <Accordion>
+          <AccordionSummary>
+            <Typography>表示項目の一覧はこちら</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <List>
+              <ListItem>
+                <ListItemText primary="レア度" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="名前" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="武器種" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="遮蔽物" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="役割" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="ポジション" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="クラス" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="学校" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="攻撃属性" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="防御属性" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="市街" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="屋外" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="屋内" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="射程距離" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="装備1" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="装備2" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="装備3" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="実装日" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="学年" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="部活" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="年齢" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="誕生日" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="身長" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="HP" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="攻撃力" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="治癒力" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="命中値" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="会心値" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="安定値" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="CC強化力" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="会心ダメージ" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="CC抵抗力" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="防御力" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="回避値" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="防御貫通値" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="コスト回復力" />
+              </ListItem>
+            </List>
+          </AccordionDetails>
+        </Accordion>
       </section>
 
       <section>
         <Typography variant="h5" gutterBottom>
           グラフについて
         </Typography>
-        <Typography>
-        </Typography>
+        <Typography>各項目ごとのデータ分布をグラフで表示します。</Typography>
+        <List>
+          <ListItem>
+            <ListItemText primary="グラフはテーブルと連動していて、テーブルでフィルタリングを行うとグラフにも反映されます。" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="名前やHPなど生徒ごとに唯一の値になっている項目はグラフに表示されません。" />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="また、学校, 学年, 武器種, 部活, 年齢, 誕生日,
+          身長については通常衣装と別衣装は同じ生徒としてカウントしています。"
+            />
+          </ListItem>
+        </List>
       </section>
 
       <section>
@@ -97,24 +238,28 @@ export function Usage() {
         <Typography variant="h5" gutterBottom>
           出典・お借りしたもの
         </Typography>
-        <Typography>
-          <Link
-            href="https://bluearchive.wikiru.jp/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ブルーアーカイブ（ブルアカ）攻略有志Wiki
-          </Link>
-          ：正とデータを参照させてもらいました。
-          <Link
-            href="https://tmp.nulla.top/ba-logo/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            BlueArchive-Style Logo Generator
-          </Link>
-          ：ロゴに使用させていただきました。
-        </Typography>
+        <List>
+          <ListItem>
+            <Link
+              href="https://bluearchive.wikiru.jp/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ブルーアーカイブ（ブルアカ）攻略有志Wiki
+            </Link>
+            ：生徒データを参照させてもらいました。
+          </ListItem>
+          <ListItem>
+            <Link
+              href="https://tmp.nulla.top/ba-logo/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              BlueArchive-Style Logo Generator
+            </Link>
+            ：ロゴに使用させていただきました。
+          </ListItem>
+        </List>
       </section>
 
       <section>
